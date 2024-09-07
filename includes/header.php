271147c0bea83c7dcb0a50588c6c0a69971fa60f
Relentlesskenn <qyan_1999@proton.me>
2024-09-08 07:23:09 +0800

    -Flow

--- includes/header.php
+++ includes/header.php
@@ -4,6 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet">
+    <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css" rel="stylesheet">
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.3/font/bootstrap-icons.min.css">
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
--- includes/navbar.php
+++ includes/navbar.php
@@ -1,9 +1,38 @@
-<nav class="navbar fixed-bottom bg-body-tertiary">
-    <form class="container-fluid justify-content-center">
-        <a class="btn btn-outline-primary btn-sm me-1" href="goals-page.php">Goals</a>
-        <a class="btn btn-outline-primary btn-sm" href="reports-page.php">Reports</button>
-        <a class="btn btn-primary btn-lg mx-2" style="border-radius: 100%" href="create-page.php">+</a>
-        <a class="btn btn-outline-primary btn-sm me-1" href="learn-page.php">Learn</a>
-        <a class="btn btn-outline-primary btn-sm" href="settings-page.php">Settings</a>
-    </form>
+<link rel="stylesheet" href=".\assets\css\navbar.css">
+<nav class="bottom-nav">
+        <div class="container">
+            <div class="row align-items-center">
+                <div class="col nav-item">
+                    <a href="goals-page.php" style="color: black; text-decoration: none;">
+                        <i class="bi bi-bullseye"></i>
+                        Goals
+                    </a>
+                </div>
+                <div class="col nav-item">
+                    <a href="reports-page.php" style="color: black; text-decoration: none;">
+                        <i class="bi bi-clipboard2-data"></i>
+                        Reports
+                    </a>
+                </div>
+                <div class="col">
+                    <a href="create-page.php" style="color: black; text-decoration: none;">
+                    <div class="add-button">
+                        <i class="bi bi-plus"></i>
+                    </div>
+                    </a>
+                </div>
+                <div class="col nav-item">
+                    <a href="learn-page.php" style="color: black; text-decoration: none;">
+                        <i class="bi bi-book"></i>
+                        Learn
+                    </a>
+                </div>
+                <div class="col nav-item">
+                    <a href="settings-page.php" style="color: black; text-decoration: none;">
+                        <i class="bi bi-gear"></i>
+                        Settings
+                    </a>
+                </div>
+            </div>
+        </div>
 </nav>
