a5445fe7226a26944df59dee53c6b371e23da6ff
Relentlesskenn <qyan_1999@proton.me>
2024-09-27 05:54:29 +0800

    fix: Remove close button from comment modal

--- reports-page.php
+++ reports-page.php
@@ -99,7 +99,6 @@ $totalPages = ceil($rowCount['total'] / $perPage);
     <div class="modal-content">
       <div class="modal-header">
         <h5 class="modal-title" id="commentModalLabel">Comment</h5>
-        <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
       </div>
       <div class="modal-body">
         <p id="commentContent"></p>
