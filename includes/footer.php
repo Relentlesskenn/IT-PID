395ab1944595b9ec1a52930c53016a89808bc21c
Relentlesskenn <qyan_1999@proton.me>
2024-10-05 07:19:34 +0800

    Bootstrap runs locally

--- includes/footer.php
+++ includes/footer.php
@@ -1,5 +1,5 @@
 
 
-<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"></script>
+<script src="assets/js/bootstrap/bootstrap.bundle.min.js"></script>
 </body>
 </html>
