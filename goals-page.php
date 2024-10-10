92985b722d6dc860188182e056321b40acb89947
Relentlesskenn <qyan_1999@proton.me>
2024-10-10 09:00:42 +0800

    Fixed Inconsistency

--- goals-page.php
+++ goals-page.php
@@ -7,10 +7,12 @@ include('includes/navbar.php');
 
 <div class="py-3">
     <div class="container">
-        <div class="d-flex justify-content-between align-items-center mb-4">
-            <a href="dashboard-page.php" class="btn btn-outline-dark">
-                <- Dashboard
-            </a>
+        <div class="container">
+            <div class="d-flex justify-content-between align-items-center mb-4">
+                <a href="dashboard-page.php" class="btn btn-outline-custom btn-sm">
+                    <i class="bi bi-arrow-left"></i> Dashboard
+                </a>
+            <h1 class="h4 mb-0">Goals</h1>
         </div>
     </div>
 </div>
