57e54b02d3926f86e137625fd3ceaebb210286ce
Jodel Saragena <jodelsaragena19@gmail.com>
2024-10-10 09:25:29 +0800

    UI added

--- index.php
+++ index.php
@@ -3,25 +3,25 @@ $page_title = "IT-PID - Budget Smart, Live Well";
 include('includes/header.php');
 ?>
 <link rel="stylesheet" href="./assets/css/landing_page.css">
-<link rel="stylesheet" href="./assets/css/bg.css">
 
-<main class="py-5">
-    <div class="container">
+
+<main class="d-flex flex-column min-vh-100 py-5 main">
+    <div class="container flex-grow-1 d-flex justify-content-center align-items-center">
         <div class="row justify-content-center">
             <div class="col-md-6 text-center">
                 <div class="logo-container mb-4">
                     <h1 class="logo-text">
-                        <span class="large-text">IT</span>
+                        <span class="large-text" style="float: left; margin-left: 3px;">IT</span>
                         <span class="large-text">PID</span>
                     </h1>
                     <p class="tagline">Budget Smart, Live Well</p>
                 </div>
-                <div class="cta-buttons">
-                    <a class="btn btn-success btn-lg mb-3 w-75" href="registration-page-1.php">Create an account</a>
-                    <a class="btn btn-dark btn-lg w-75" href="login-page.php">Log in</a>
-                </div>
             </div>
         </div>
+    </div>
+    <div class="cta-buttons text-center mb-3">
+        <a class="btn btn-custom-primary btn-lg mb-3 w-100 mt-auto" href="registration-page-1.php">Create an account</a>
+        <a class="btn btn-light btn-lg w-100 mt-auto" href="login-page.php">Log in</a>
     </div>
 </main>
 
