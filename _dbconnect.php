89b20de198f28469d5bd180fd3789a8ffd8fd78e
Relentlesskenn <qyan_1999@proton.me>
2024-10-12 12:08:51 +0800

    Using prepared statements

--- _dbconnect.php
+++ _dbconnect.php
@@ -1,6 +1,11 @@
 <?php
+$host = "localhost";
+$user = "root";
+$pass = "";
+$db = "it-pid";
 
-// Database Connection 
-$conn = mysqli_connect("localhost","root","","it-pid");
+$conn = new mysqli($host, $user, $pass, $db);
 
-?>
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
